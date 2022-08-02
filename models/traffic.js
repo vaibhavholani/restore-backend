21ea110b0d20aaf2c3b2cd657c2822774c55ffef
Vaibhav Holani <vaibhavholani@gmail.con>
2022-08-02 15:57:07 -0400

    enforcing index creation try2

--- models/traffic.js
+++ models/traffic.js
@@ -4,7 +4,7 @@ const TrafficSchema = new mongoose.Schema({
     ip_address: {type: String, required: true}, 
     date: {type: String, required: true },
 })
-mongoose.connection.collections["traffics"].ensureIndex( { ip_address: 1, date: 1}, { unique: true, dropDups: true} )
+mongoose.connection.db.collections["traffics"].ensureIndex( { ip_address: 1, date: 1}, { unique: true, dropDups: true} )
 
 const Traffic = mongoose.model('Traffic', TrafficSchema)
 
